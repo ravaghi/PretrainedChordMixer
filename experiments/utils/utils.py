4ca14c8ea2adf0b77ea62d5286ed0b4f65cff65b
Mahdi Ravaghi <mahdi.hasani@ntnu.no>
2022-10-11 16:35:47 +0200

    Set random seed

--- experiments/utils/utils.py
+++ experiments/utils/utils.py
@@ -2,6 +2,8 @@ from sklearn.utils.class_weight import compute_class_weight
 from omegaconf import OmegaConf
 from torch import nn
 import pandas as pd
+import numpy as np
+import random
 import torch
 import wandb
 import os
@@ -9,10 +11,21 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
+def seed_everything(seed=42):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+
+
 def init_run(config):
+    seed_everything(config.general.seed)
+
     print("-" * 20 + " configs " + "-" * 20)
     print(OmegaConf.to_yaml(config))
     print("-" * 20 + " configs " + "-" * 20)
+
     device = f'cuda:{config.general.device_id}' if torch.cuda.is_available() else 'cpu'
     print(f'Using device: {device}')
 
