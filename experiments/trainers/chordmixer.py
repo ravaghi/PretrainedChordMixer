2cabe903a67cd6e29d42c92f7e9c0fb0f58fe038
ravaghi <mahdi.hasani@ntnu.no>
2022-10-21 15:46:05 +0200

    Add roc_auc metric

--- experiments/trainers/chordmixer.py
+++ experiments/trainers/chordmixer.py
@@ -1,4 +1,5 @@
 from tqdm import tqdm
+from sklearn import metrics
 import torch
 import wandb
 
@@ -18,9 +19,12 @@ class ChordMixerTrainer:
         
         num_batches = len(self.train_dataloader)
         
-        running_loss = 0
+        running_loss = 0.0
         correct = 0
         total = 0
+        
+        preds = []
+        targets = []
 
         loop = tqdm(enumerate(self.train_dataloader), total=num_batches)
         for idx, (x, y, seq_len, _bin) in loop:
@@ -43,6 +47,9 @@ class ChordMixerTrainer:
             _, predicted = y_hat.max(1)
             total += y.size(0)
             correct += predicted.eq(y).sum().item()
+            
+            targets.extend(y.detach().cpu().numpy().flatten())
+            preds.extend(predicted.detach().cpu().numpy().flatten())
 
             loop.set_description(f'Epoch {current_epoch_nr + 1}')
             loop.set_postfix(train_acc=round(correct / total, 2),
@@ -52,19 +59,24 @@ class ChordMixerTrainer:
             #     wandb.log({'train_loss': running_loss / (idx + 1)})
             #     wandb.log({'train_accuracy': correct / total})
 
+        train_auc = metrics.roc_auc_score(targets, preds)
         train_accuracy = correct / total
         train_loss = running_loss / num_batches
         wandb.log({'train_loss': train_loss}, step=current_epoch_nr)
         wandb.log({'train_accuracy': train_accuracy}, step=current_epoch_nr)
+        wandb.log({'train_auc': train_auc}, step=current_epoch_nr)
 
     def evaluate(self, current_epoch_nr):
         self.model.eval()
 
         num_batches = len(self.val_dataloader)
 
-        running_loss = 0
+        running_loss = 0.0
         correct = 0
         total = 0
+        
+        preds = []
+        targets = []
 
         with torch.no_grad():
             loop = tqdm(enumerate(self.val_dataloader), total=num_batches)
@@ -84,6 +96,9 @@ class ChordMixerTrainer:
                 _, predicted = y_hat.max(1)
                 total += y.size(0)
                 correct += predicted.eq(y).sum().item()
+                
+                targets.extend(y.detach().cpu().numpy().flatten())
+                preds.extend(predicted.detach().cpu().numpy().flatten())
 
                 loop.set_description(f'Epoch {current_epoch_nr + 1}')
                 loop.set_postfix(val_acc=round(correct / total, 2),
@@ -93,7 +108,9 @@ class ChordMixerTrainer:
                 #     wandb.log({'val_loss': running_loss / (idx + 1)})
                 #     wandb.log({'val_accuracy': correct / total})
 
+        val_auc = metrics.roc_auc_score(targets, preds)
         validation_accuracy = correct / total
         validation_loss = running_loss / num_batches
         wandb.log({'val_loss': validation_loss}, step=current_epoch_nr)
         wandb.log({'val_accuracy': validation_accuracy}, step=current_epoch_nr)
+        wandb.log({'val_auc': val_auc}, step=current_epoch_nr)
