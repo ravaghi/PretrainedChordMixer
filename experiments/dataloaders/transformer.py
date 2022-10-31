092ce4bbf1a3de571aa608416ed614a497735f5e
ravaghi <mahdi.hasani@ntnu.no>
2022-10-31 14:41:45 +0100

    Add transformer model

--- experiments/dataloaders/transformer.py
+++ experiments/dataloaders/transformer.py
@@ -1,4 +1,4 @@
-from torch.utils.data import TensorDataset, DataLoader
+from torch.utils.data import Dataset, DataLoader
 from Bio import SeqIO
 import pandas as pd
 import numpy as np
@@ -9,7 +9,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 
 DNA_BASE_DICT = {'A': 0, 'C': 1, 'G': 2, 'T': 3, 'N': 4, 'Y': 5, 'R': 6, 'M': 7, 'W': 8, 'K': 9, 'S': 10, 'B': 11, 'H': 12, 'D': 13, 'V': 14}
 DNA_BASE_DICT_REVERSED = {0: 'A', 1: 'C', 2: 'G', 3: 'T', 4: 'N', 5: 'Y', 6: 'R', 7: 'M', 8: 'W', 9: 'K', 10: 'S', 11: 'B', 12: 'H', 13: 'D', 14: 'V'}
-
 
 def pad_sequences(dataframe, max_len=1000):
     max_seq_len = dataframe["sequence"].apply(lambda x: len(x)).max()
@@ -37,19 +36,24 @@ def process_taxonomy_classification_dataframe(dataframe, dataset_name):
     dataframe = pad_sequences(dataframe, max_len)
     dataframe = convert_base_to_index(dataframe)
     dataframe["len"] = dataframe["sequence"].apply(lambda x: len(x))
-    print(dataframe)
-    return dataframe[["sequence", "label"]]
-
-def process_variant_effect_prediction_dataframe(dataframe):
-    sequence_path = os.path.join(BASE_DIR, "data", "variant_effect_prediction", "hg38.fa")
-    sequences = SeqIO.to_dict(SeqIO.parse(sequence_path, "fasta"))
-    dataframe["sequence"] = dataframe.apply(lambda x: str(sequences[x.chr].seq[x.pos-20_000:x.pos+20_000]), axis=1)  
-    dataframe = pad_sequences(dataframe, "constant") 
-    dataframe = convert_base_to_index(dataframe)
     return dataframe[["sequence", "label"]]
 
 
-class CNNDataLoader:
+class DatasetCreator(Dataset):
+    def __init__(self, df):
+        self.df = df
+
+    def __getitem__(self, index):
+        X, Y = self.df.iloc[index, :]
+        Y = torch.tensor(Y)
+        X = torch.tensor(X)
+        return X, Y
+
+    def __len__(self):
+        return len(self.df)
+
+
+class TransformerDataLoader:
     def __init__(self, data_path, dataset, dataset_name, batch_size):
         self.data_path = data_path
         self.dataset = dataset
@@ -62,20 +66,12 @@ class CNNDataLoader:
         if "taxonomy" in self.dataset_name.lower():
             dataframe = pd.read_pickle(data_path)
             dataframe = process_taxonomy_classification_dataframe(dataframe, self.dataset_name)
-        elif "variant" in self.dataset_name.lower():
-            dataframe = pd.read_csv(data_path)
-            dataframe = process_variant_effect_prediction_dataframe(dataframe)
         else:
             raise ValueError(f"Dataset {self.dataset_name} name not recognized")
 
-        sequences = np.array(dataframe.sequence.values.tolist(), dtype=np.float32)
-        labels = dataframe.label.values.tolist()
-
-        sequences = torch.tensor(sequences)
-        labels = torch.tensor(labels)
-        
-        dataset = TensorDataset(sequences, labels)
-        
+        dataset = DatasetCreator(
+            df=dataframe
+        )
         return DataLoader(
             dataset,
             batch_size=self.batch_size,
--- experiments/utils/utils.py
+++ experiments/utils/utils.py
@@ -10,6 +10,9 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+DNA_BASE_DICT = {'A': 0, 'C': 1, 'G': 2, 'T': 3, 'N': 4, 'Y': 5, 'R': 6, 'M': 7, 'W': 8, 'K': 9, 'S': 10, 'B': 11, 'H': 12, 'D': 13, 'V': 14}
+DNA_BASE_DICT_REVERSED = {0: 'A', 1: 'C', 2: 'G', 3: 'T', 4: 'N', 5: 'Y', 6: 'R', 7: 'M', 8: 'W', 9: 'K', 10: 'S', 11: 'B', 12: 'H', 13: 'D', 14: 'V'}
+
 
 def seed_everything(seed=42):
     random.seed(seed)
@@ -22,18 +25,18 @@ def seed_everything(seed=42):
 def init_run(config):
     seed_everything(config.general.seed)
 
+    wandb.init(project=config.wandb.project,
+                entity=config.wandb.entity,
+                config=OmegaConf.to_container(config, resolve=True),
+                name=config.wandb.name.split(".")[-1],
+                dir=BASE_DIR)
+
     print("-" * 30 + " config " + "-" * 30)
     print(OmegaConf.to_yaml(config))
     print("-" * 30 + " config " + "-" * 30)
 
     device = f'cuda:{config.general.device_id}' if torch.cuda.is_available() else 'cpu'
     print(f'Using device: {device}')
-
-    wandb.init(project=config.wandb.project,
-               entity=config.wandb.entity,
-               config=OmegaConf.to_container(config, resolve=True),
-               name=config.wandb.name.split(".")[-1],
-               dir=BASE_DIR)
 
     return device
 
