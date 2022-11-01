57b2ed3cae4cc3f5477b9cb4e84f294a8f59ab70
ravaghi <mahdi.hasani@ntnu.no>
2022-11-01 16:21:23 +0100

    Add linformer

---
+++ experiments/models/linformer.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+from linformer import Linformer as LinformerModel
+
+class Linformer(nn.Module):
+    def __init__(self, vocab_size, embedding_size, num_heads, num_layers, const_vector_length, n_class, pooling, device_id):
+        super(Linformer, self).__init__()
+        self.device = f"cuda:{device_id}" if torch.cuda.is_available() else "cpu"
+        self.encoder = nn.Embedding(vocab_size, embedding_size)
+        self.posenc = nn.Embedding(const_vector_length, embedding_size)
+        self.linformermodel = LinformerModel(
+            dim=embedding_size,
+            seq_len=const_vector_length,
+            depth=num_layers,
+            heads=num_heads,
+            k=256,
+            one_kv_head=True,
+            share_kv=True
+        )
+        self.pooling = pooling
+        self.const_vector_length = const_vector_length
+        self.final = nn.Linear(embedding_size, n_class)
+        if self.pooling == 'flatten':         
+            self.final = nn.Linear(embedding_size*const_vector_length, n_class)
+        self.linear = nn.Linear(2, embedding_size, bias=True)
+
+    def forward(self, x):
+        x = self.encoder(x)
+        positions = torch.arange(0, self.const_vector_length).expand(x.size(0), self.const_vector_length).to(self.device)
+        x = self.posenc(positions) + x
+        x = self.linformermodel(x)
+        if self.pooling == 'avg':
+            x = torch.mean(x, 1)
+        elif self.pooling == 'cls':
+            x = x[:, 0, :]
+        x = self.final(x.view(x.size(0), -1))
+        return x
---
+++ experiments/trainers/linformer.py
@@ -0,0 +1,110 @@
+from tqdm import tqdm
+from sklearn import metrics
+import torch
+import wandb
+
+
+class LinformerTrainer:
+    def __init__(self, model, train_dataloader, val_dataloader, device, criterion, optimizer, log_every_n_steps):
+        self.model = model
+        self.train_dataloader = train_dataloader
+        self.val_dataloader = val_dataloader
+        self.device = device
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.log_every_n_steps = log_every_n_steps
+        
+    def train(self, current_epoch_nr):
+        self.model.train()
+        
+        num_batches = len(self.train_dataloader)
+        
+        running_loss = 0.0
+        correct = 0
+        total = 0
+        
+        preds = []
+        targets = []
+
+        loop = tqdm(enumerate(self.train_dataloader), total=num_batches)
+        for idx, (x, y) in loop:
+            x = x.to(self.device)
+            y = y.to(self.device)
+
+            y_hat = self.model(x)
+                
+            loss = self.criterion(y_hat, y)
+            loss.backward()
+
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+            
+            running_loss += loss.item()
+
+            _, predicted = y_hat.max(1)
+            total += y.size(0)
+            correct += predicted.eq(y).sum().item()
+            
+            targets.extend(y.detach().cpu().numpy().flatten())
+            preds.extend(predicted.detach().cpu().numpy().flatten())
+
+            loop.set_description(f'Epoch {current_epoch_nr + 1}')
+            loop.set_postfix(train_acc=round(correct / total, 2),
+                             train_loss=round(running_loss / total, 2))
+            
+            # if (idx + 1) % self.log_every_n_steps == 0:
+            #     wandb.log({'train_loss': running_loss / (idx + 1)})
+            #     wandb.log({'train_accuracy': correct / total})
+
+        train_auc = metrics.roc_auc_score(targets, preds)
+        train_accuracy = correct / total
+        train_loss = running_loss / num_batches
+        wandb.log({'train_loss': train_loss}, step=current_epoch_nr)
+        wandb.log({'train_accuracy': train_accuracy}, step=current_epoch_nr)
+        wandb.log({'train_auc': train_auc}, step=current_epoch_nr)
+
+    def evaluate(self, current_epoch_nr):
+        self.model.eval()
+
+        num_batches = len(self.val_dataloader)
+
+        running_loss = 0.0
+        correct = 0
+        total = 0
+        
+        preds = []
+        targets = []
+
+        with torch.no_grad():
+            loop = tqdm(enumerate(self.val_dataloader), total=num_batches)
+            for idx, (x, y) in loop:
+                x = x.to(self.device)
+                y = y.to(self.device)
+                    
+                y_hat = self.model(x)
+                
+                loss = self.criterion(y_hat, y)
+                
+                running_loss += loss.item()
+
+                _, predicted = y_hat.max(1)
+                total += y.size(0)
+                correct += predicted.eq(y).sum().item()
+                
+                targets.extend(y.detach().cpu().numpy().flatten())
+                preds.extend(predicted.detach().cpu().numpy().flatten())
+
+                loop.set_description(f'Epoch {current_epoch_nr + 1}')
+                loop.set_postfix(val_acc=round(correct / total, 2),
+                                 val_loss=round(running_loss / total, 2))
+                
+                # if (idx + 1) % self.log_every_n_steps == 0:
+                #     wandb.log({'val_loss': running_loss / (idx + 1)})
+                #     wandb.log({'val_accuracy': correct / total})
+
+        val_auc = metrics.roc_auc_score(targets, preds)
+        validation_accuracy = correct / total
+        validation_loss = running_loss / num_batches
+        wandb.log({'val_loss': validation_loss}, step=current_epoch_nr)
+        wandb.log({'val_accuracy': validation_accuracy}, step=current_epoch_nr)
+        wandb.log({'val_auc': val_auc}, step=current_epoch_nr)
