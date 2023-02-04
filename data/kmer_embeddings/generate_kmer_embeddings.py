989bb379ee1654e7da524359ce04e33e8a889eea
Mahdi Ravaghi <mahdi.hasani@ntnu.no>
2023-02-04 11:18:00 +0100

    Optimize kmer embedding generation

--- data/kmer_embeddings/generate_kmer_embeddings.py
+++ data/kmer_embeddings/generate_kmer_embeddings.py
@@ -7,21 +7,40 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
-def get_sequences_list():
+def get_sequences_list(fasta_file_path: str, max_sequence_length: int) -> list:
+    """
+    Collects sequences from a fasta file and returns them as a list of strings.
+
+    Args:
+        fasta_file_path: Path to the fasta file.
+
+    Returns:
+        A list of strings, each string is a sequence.
+    """
+    sequences = SeqIO.parse(fasta_file_path, "fasta")
+
     sequences_list = []
-    sequences = SeqIO.parse(BASE_DIR + "/data/variant_effect_prediction/hg38.fa", "fasta")
-    print("Reading sequences...")
-    for record in sequences:
-        sequence_str = str(record.seq).upper()
-        if len(sequence_str) < 1000_000:
+    for sequence in sequences:
+        sequence_str = str(sequence.seq).upper().replace("N", "")
+        if len(sequence_str) < max_sequence_length:
             sequences_list.append(sequence_str)
         else:
-            sequences_list.append(sequence_str[:1000_000])
-
+            sequences_list.append(sequence_str[:max_sequence_length])
     return sequences_list
 
 
-def generate_kmers(sequences, kmer_length, stride):
+def generate_kmers(sequences: list, kmer_length: int, stride: int) -> list:
+    """
+    Generates kmers from a list of sequences.
+
+    Args:
+        sequences: A list of strings, each string is a sequence.
+        kmer_length: The length of the kmers to generate.
+        stride: The stride to use when generating kmers.
+
+    Returns:
+        A list of lists, each list contains the kmers for a sequence.
+    """
     kmers = []
     for sequence in tqdm(sequences):
         temp_kmers = []
@@ -31,7 +50,20 @@ def generate_kmers(sequences, kmer_length, stride):
     return kmers
 
 
-def generate_kmer_embeddings(sequences, kmer_length, stride, embedding_size, epochs):
+def generate_kmer_embeddings(sequences: list, kmer_length: int, stride: int, embedding_size: int, epochs: int) -> None:
+    """
+    Generates kmer embeddings using gensim's Word2Vec model and save.
+
+    Args:
+        sequences: A list of strings, each string is a sequence.
+        kmer_length: The length of the kmers to generate.
+        stride: The stride to use when generating kmers.
+        embedding_size: The size of the embedding vectors.
+        epochs: The number of epochs to train the model for.
+
+    Returns:
+        None
+    """
     model_name = "K{}_S{}_L{}.model".format(kmer_length, stride, embedding_size)
     kmers = generate_kmers(sequences, kmer_length, stride)
     model = gensim.models.Word2Vec(kmers, vector_size=embedding_size, workers=multiprocessing.cpu_count() - 1)
@@ -40,8 +72,15 @@ def generate_kmer_embeddings(sequences, kmer_length, stride, embedding_size, epo
     model.save(model_name)
 
 
+# Best parameters
+# Kmer length: 5
+# Stride: 2
+# Embedding size: 50
 if __name__ == "__main__":
-    sequences = get_sequences_list()
+    _FASTA_FILE_PATH = BASE_DIR + "/data/variant_effect_prediction/hg38.fa"
+    _MAX_SEQUENCE_LENGTH = 1000_000
+
+    sequences = get_sequences_list(_FASTA_FILE_PATH, _MAX_SEQUENCE_LENGTH)
     for kmer_length in [5, 4, 6]:
         for stride in [2, 1, 3]:
             for embedding_size in [50, 80, 100]:
